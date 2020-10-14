48d367d07a1260092ba621e8a252dba87796008b
Maxim Zakharov <ekb.maxi@gmail.com>
2020-10-14 22:07:24 +0500

    Lesson3: task 2

--- JS_Level1/Lesson3/main.js
+++ JS_Level1/Lesson3/main.js
@@ -27,22 +27,42 @@ function drawPyramid(rows) {
   }
 }
 
-// Task 1
+function countCartTotal() {}
+
+// ! Task 1
 console.log("-------TASK 1-------");
 displaySimpleNumbers(
   prompt("Введите число, до какого хотите вывести все простые числа")
 );
 
-// Task 2
+// ! Task 2
 console.log("-------TASK 2-------");
-// Предположим, есть сущность корзины. Нужно реализовать функционал подсчета стоимости корзины в зависимости от находящихся в ней товаров. Товары в корзине хранятся в массиве. Задачи:
-// a) Организовать такой массив для хранения товаров в корзине;
-// b) Организовать функцию countBasketPrice, которая будет считать стоимость корзины.
 
-// Task 3
+const item1 = { name: "Socks", price: 200 };
+const item2 = { name: "Underwear", price: 300 };
+
+const cart = {
+  items: [],
+  total: function () {
+    let sum = 0;
+    this.items.forEach((item) => {
+      sum += item.price;
+    });
+    return sum;
+  },
+};
+
+cart.items.push(item1, item2);
+console.log(
+  `There are ${
+    cart.items.length
+  } items in the cart with total price '${cart.total()}'`
+);
+
+// ! Task 3
 console.log("-------TASK 3-------");
 magicForLoop();
 
-// Task 4
+// ! Task 4
 console.log("-------TASK 4-------");
 drawPyramid(prompt("Введите кол-во строк пирамиды"));
