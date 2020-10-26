51b872ceea689d53ed5eb74ef953e8e3f40b2932
Maxim Zakharov <ekb.maxi@gmail.com>
2020-10-26 16:06:26 +0500

    Lesson6: use try/catch for getting JSON from the localStorage

--- JS_Level1/Lesson6/script/cartPage.js
+++ JS_Level1/Lesson6/script/cartPage.js
@@ -1,28 +1,35 @@
 const newCart = new Cart();
-cart = JSON.parse(localStorage.getItem("cart"));
+try {
+  cart = JSON.parse(localStorage.getItem("cart"));
 
-for (const product of cart._items) {
-  newCart.addItem(
-    new Product(
-      product._name,
-      product._price,
-      product._quantity,
-      product._imgUrl
-    )
+  for (const product of cart._items) {
+    newCart.addItem(
+      new Product(
+        product._name,
+        product._price,
+        product._quantity,
+        product._imgUrl
+      )
+    );
+  }
+
+  console.log(newCart);
+
+  newCart.render();
+
+  const proceedToCheckoutDiv = document.createElement("div");
+  proceedToCheckoutDiv.classList.add("center");
+
+  const proceedToCheckoutBtn = document.createElement("input");
+  proceedToCheckoutBtn.setAttribute("type", "button");
+  proceedToCheckoutBtn.classList.add("btn__buy", "center");
+  proceedToCheckoutBtn.value = "Оформить покупку >>";
+
+  proceedToCheckoutDiv.appendChild(proceedToCheckoutBtn);
+  document.querySelector(".content").appendChild(proceedToCheckoutDiv);
+} catch (e) {
+  console.error(
+    e,
+    "Cannot parse cart's JSON from the local storage for some reason"
   );
 }
-
-console.log(newCart);
-
-newCart.render();
-
-const proceedToCheckoutDiv = document.createElement("div");
-proceedToCheckoutDiv.classList.add("center");
-
-const proceedToCheckoutBtn = document.createElement("input");
-proceedToCheckoutBtn.setAttribute("type", "button");
-proceedToCheckoutBtn.classList.add("btn__buy", "center");
-proceedToCheckoutBtn.value = "Оформить покупку >>";
-
-proceedToCheckoutDiv.appendChild(proceedToCheckoutBtn);
-document.querySelector(".content").appendChild(proceedToCheckoutDiv);
