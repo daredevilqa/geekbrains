10190407b3bb8b81a56738625249a442eed0fd46
Maxim Zakharov <ekb.maxi@gmail.com>
2020-10-27 17:10:48 +0500

    Lesson6: use main.js and render actualCart on the cart page

--- JS_Level1/Lesson6/script/cartPage.js
+++ JS_Level1/Lesson6/script/cartPage.js
@@ -1,35 +1,12 @@
-const newCart = new Cart();
-try {
-  cart = JSON.parse(localStorage.getItem("cart"));
+actualCart.render();
 
-  for (const product of cart._items) {
-    newCart.addItem(
-      new Product(
-        product._name,
-        product._price,
-        product._quantity,
-        product._imgUrl
-      )
-    );
-  }
+const proceedToCheckoutDiv = document.createElement("div");
+proceedToCheckoutDiv.classList.add("center");
 
-  console.log(newCart);
+const proceedToCheckoutBtn = document.createElement("input");
+proceedToCheckoutBtn.setAttribute("type", "button");
+proceedToCheckoutBtn.classList.add("btn__buy", "center");
+proceedToCheckoutBtn.value = "Оформить покупку >>";
 
-  newCart.render();
-
-  const proceedToCheckoutDiv = document.createElement("div");
-  proceedToCheckoutDiv.classList.add("center");
-
-  const proceedToCheckoutBtn = document.createElement("input");
-  proceedToCheckoutBtn.setAttribute("type", "button");
-  proceedToCheckoutBtn.classList.add("btn__buy", "center");
-  proceedToCheckoutBtn.value = "Оформить покупку >>";
-
-  proceedToCheckoutDiv.appendChild(proceedToCheckoutBtn);
-  document.querySelector(".content").appendChild(proceedToCheckoutDiv);
-} catch (e) {
-  console.error(
-    e,
-    "Cannot parse cart's JSON from the local storage for some reason"
-  );
-}
+proceedToCheckoutDiv.appendChild(proceedToCheckoutBtn);
+document.querySelector(".content").appendChild(proceedToCheckoutDiv);
