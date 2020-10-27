67ce930e47412dd6d06ed8710b042026fd3e88b1
Maxim Zakharov <ekb.maxi@gmail.com>
2020-10-27 19:45:12 +0500

    Lesson6: implement renders for Catalog and Product, persist Cart items counter when switching pages

--- JS_Level1/Lesson6/script/product.js
+++ JS_Level1/Lesson6/script/product.js
@@ -14,9 +14,13 @@ class Product {
 
   _imgUrl = "";
 
+  _productPageUrl = "";
+
   getOverallPrice() {
     return this._price * this._quantity * (1 - this._discount / 100);
   }
+
+  //#region Getters/Setters
 
   get name() {
     return this._name;
@@ -62,10 +66,61 @@ class Product {
     this._imgUrl = url;
   }
 
-  constructor(name, price, quantity = 1, imgUrl = "") {
+  //#endregion Getters/Setters
+
+  render(insertIntoCss = ".catalog__grid") {
+    const outerDiv = document.createElement("div");
+    outerDiv.classList.add("catalog_item");
+
+    const nameDiv = document.createElement("div");
+    nameDiv.classList.add("product__name", "item_row");
+    const nameLink = document.createElement("a");
+    nameLink.setAttribute("href", this._productPageUrl);
+    const bold = document.createElement("b");
+    bold.innerText = this._name;
+    nameDiv.appendChild(nameLink).appendChild(bold);
+
+    const imgDiv = document.createElement("div");
+    imgDiv.classList.add("item_row");
+    const imgLink = document.createElement("a");
+    imgLink.setAttribute("href", this._productPageUrl);
+    const img = document.createElement("img");
+    img.classList.add("product__img");
+    img.src = this._imgUrl;
+    img.alt = this._name;
+    img.title = this._name;
+    img.width = 200;
+    imgDiv.appendChild(imgLink).appendChild(img);
+
+    const priceAndCurrencyDiv = document.createElement("div");
+    priceAndCurrencyDiv.classList.add("item_row");
+    const priceDiv = document.createElement("div");
+    priceDiv.classList.add("product__price", "price");
+    priceDiv.innerText = this._price;
+    const currencyDiv = document.createElement("div");
+    currencyDiv.classList.add("currency");
+    currencyDiv.innerText = "РУБ.";
+    priceAndCurrencyDiv.append(priceDiv, currencyDiv);
+
+    const buyBtnDiv = document.createElement("div");
+    buyBtnDiv.classList.add("item_row");
+    const buyBtn = document.createElement("input");
+    buyBtn.classList.add("btn__buy");
+    buyBtn.setAttribute("type", "button");
+    buyBtn.setAttribute("value", "Купить");
+    buyBtn.addEventListener("click", () => addToCart(this));
+    buyBtnDiv.appendChild(buyBtn);
+
+    outerDiv.append(nameDiv, imgDiv, priceAndCurrencyDiv, buyBtnDiv);
+
+    document.querySelector(insertIntoCss).appendChild(outerDiv);
+  }
+
+  constructor(name, price, imgUrl = "", productPageUrl = "", quantity = 1) {
     this._name = name;
     this._price = price;
-    this._quantity = quantity;
     this._imgUrl = imgUrl;
+    this._productPageUrl = productPageUrl;
+    this._quantity = quantity;
   }
 }
